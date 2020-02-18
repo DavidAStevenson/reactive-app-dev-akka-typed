a1d2bae52516348eae622b28fe6940e870555dc1
David Stevenson <david.35472@gmail.com>
2020-02-18 22:17:01 +0900

    Add limited oo-style actor behavior

--- src/main/scala/Tourist.scala
+++ src/main/scala/Tourist.scala
@@ -1,4 +1,16 @@
+import akka.actor.typed.{ Behavior }
+import akka.actor.typed.scaladsl.{ AbstractBehavior, ActorContext }
+
 object Tourist {
   sealed trait Command
   case class Start(codes: Seq[String]) extends Command
+}
+
+import Tourist.Start
+class Tourist(context: ActorContext[Start]) extends AbstractBehavior[Start](context) {
+
+  override def onMessage(msg: Start): Behavior[Start] = {
+    println("Received a Start message")
+    this
+  }
 }
