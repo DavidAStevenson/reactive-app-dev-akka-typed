f72b6464200b064fc586bbdff3d4dbfcf60d2e40
David Stevenson <david.35472@gmail.com>
2020-02-21 21:29:50 +0900

    Make Guidebook's response better

--- src/main/scala/Guidebook.scala
+++ src/main/scala/Guidebook.scala
@@ -1,6 +1,8 @@
 import akka.actor.typed.{ ActorRef, Behavior }
 import akka.actor.typed.scaladsl.{ AbstractBehavior, ActorContext, Behaviors }
 
+import java.util.{Currency, Locale}
+
 object Guidebook {
   def apply(): Behavior[Command] = Behaviors.setup(context => new Guidebook(context))
 
@@ -13,10 +15,21 @@ object Guidebook {
 
 import Guidebook._
 private class Guidebook(context: ActorContext[Command]) extends AbstractBehavior[Command](context) {
+  def describe(locale: Locale) =
+    s"""In ${locale.getDisplayCountry},
+      ${locale.getDisplayCountry} is spoken and the currency
+      is the ${Currency.getInstance(locale).getDisplayName}"""
+
   override def onMessage(msg: Command): Behavior[Command] = {
     println(s"${context.self}: Received a Command: ${msg}")
     msg match {
-      case Inquiry(countryCode, replyTo) => replyTo ! Guidance(countryCode, "awesome")
+      case Inquiry(countryCode, replyTo) =>
+        println(s"Actor ${context.self} responding to inquiry about $countryCode")
+        Locale.getAvailableLocales.
+        filter(_.getCountry == countryCode).
+        foreach { locale =>
+          replyTo ! Guidance(countryCode, describe(locale))
+        }
     }
     this
   }
