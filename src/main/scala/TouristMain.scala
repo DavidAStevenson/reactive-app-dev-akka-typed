9425c6bfc2710ef13845c64c42f02d48954e66d0
David Stevenson <david.35472@gmail.com>
2020-02-24 21:10:28 +0900

    Remove unused code

--- src/main/scala/TouristMain.scala
+++ src/main/scala/TouristMain.scala
@@ -10,39 +10,21 @@ object TourismWorld {
   sealed trait Command
   private case class ListingResponse(listing: Receptionist.Listing) extends Command
 
-  //def apply(): Behavior[Command] =
-  //  Behaviors.setup[Command] { context =>
   def apply(): Behavior[Nothing] =
     Behaviors.setup[Receptionist.Listing] { context =>
-      //val listingResponseAdapter = context.messageAdapter[Receptionist.Listing](ListingResponse)
 
-      // context.system.receptionist ! Receptionist.Find(Guidebook.GuidebookServiceKey, listingResponseAdapter)
-      // println(s"Trying to find Guidebook with ${Guidebook.GuidebookServiceKey}")
       context.system.receptionist ! Receptionist.Subscribe(Guidebook.GuidebookServiceKey, context.self)
       println(s"Subscribing to find Guidebook with ${Guidebook.GuidebookServiceKey}")
 
       Behaviors.receiveMessagePartial[Receptionist.Listing] {
         case Guidebook.GuidebookServiceKey.Listing(listings) =>
           listings.foreach { guidebook =>
-              //context.spawn(Tourist(guidebook), "dave")
             println(s"Spawning tourist with ${guidebook}")
             val tourist = context.spawn(Tourist(guidebook), "dave")
             tourist ! Tourist.Start((Locale.getISOCountries).toIndexedSeq)
           }
           Behaviors.same
       }
-
-      /*
-      Behaviors.receiveMessage {
-        case ListingResponse(Guidebook.GuidebookServiceKey.Listing(listings)) =>
-          listings.foreach { guidebook => 
-            println(s"Spawning tourist with ${guidebook}")
-            val tourist = context.spawn(Tourist(guidebook), "dave")
-            tourist ! Tourist.Start((Locale.getISOCountries).toIndexedSeq)
-          }
-          Behaviors.same
-      }
-      */
     }.narrow
 
 }
