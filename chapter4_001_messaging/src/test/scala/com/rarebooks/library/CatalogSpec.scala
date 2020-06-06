eab615cdffab13b1308f628ffd4ef67560e2291b
David Stevenson <david.35472@gmail.com>
2020-06-06 21:57:34 +0900

    Add start of Catalog simulator with tests

---
+++ chapter4_001_messaging/src/test/scala/com/rarebooks/library/CatalogSpec.scala
@@ -0,0 +1,18 @@
+package com.rarebooks.library
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+
+class CatalogSpec extends AnyWordSpec with Matchers {
+
+  import Catalog._
+  import RareBooksProtocol._
+
+  "books" should {
+    "contain some example books" in {
+      books.values.toSet should === (Set[BookCard](theEpicOfGilgamesh, phaedrus, theHistories))
+    }
+  }
+
+}
+
