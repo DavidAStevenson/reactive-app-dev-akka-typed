9399c95ee79f48b5ca63483af56a2cedbf800ed8
David Stevenson <david.35472@gmail.com>
2020-06-10 22:07:15 +0900

    add test of RareBooks opening/closing on schedule

--- chapter4_001_messaging/src/test/scala/com/rarebooks/library/RareBooksSpec.scala
+++ chapter4_001_messaging/src/test/scala/com/rarebooks/library/RareBooksSpec.scala
@@ -1,9 +1,11 @@
 package com.rarebooks.library
 
+import scala.concurrent.duration._
 import akka.actor.testkit.typed.CapturedLogEvent
 import akka.actor.testkit.typed.scaladsl.BehaviorTestKit
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 import akka.actor.testkit.typed.scaladsl.LoggingTestKit
+import akka.actor.testkit.typed.scaladsl.ManualTime
 import org.slf4j.event.Level
 import org.scalatest.wordspec.AnyWordSpecLike
 
@@ -18,33 +20,77 @@ class RareBooksSynchronousSpec extends BaseSpec {
   }
 }
 
-class RareBooksAsyncSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+class RareBooksAsyncSpec
+  extends ScalaTestWithActorTestKit(ManualTime.config)
+  with AnyWordSpecLike {
 
-  "RareBooks" should {
+  val openLog = "Time to open up!"
+  val closeLog = "Time to close!"
+  val reportLog = "Time to produce a report."
 
-    val rareBooks = testKit.spawn(RareBooks(), "rareBooks")
+  "RareBooks" can {
 
-    val openLog = "Time to open up!"
-    s"log '${openLog}' at info, when opened" in {
-      LoggingTestKit.info(openLog).expect {
-        rareBooks ! RareBooks.Open
+    "receive messages" should {
+
+      val rareBooks = testKit.spawn(RareBooks(), "rareBooks")
+
+      s"log '${openLog}' at info, when opened" in {
+        LoggingTestKit.info(openLog).expect {
+          rareBooks ! RareBooks.Open
+        }
+      }
+
+      s"log '${closeLog}' at info, when closed" in {
+        LoggingTestKit.info(closeLog).expect {
+          rareBooks ! RareBooks.Close
+        }
+      }
+
+      s"log '${reportLog}' at info, when a report command is received" in {
+        LoggingTestKit.info(reportLog).expect {
+          rareBooks ! RareBooks.Report
+        }
       }
     }
 
-    val closeLog = "Time to close!"
-    s"log '${closeLog}' at info, when closed" in {
-      LoggingTestKit.info(closeLog).expect {
-        rareBooks ! RareBooks.Close
+    "operate independently" should {
+
+      val manualTime: ManualTime = ManualTime()
+      val rareBooks = spawn(RareBooks(), "rareBooks2")
+
+      s"open up when initially commanded to open" in {
+        LoggingTestKit.info(openLog).expect {
+          rareBooks ! RareBooks.Open
+        }
+      }
+
+      "stay open for at least 9 seconds" in {
+        LoggingTestKit
+          .info(closeLog)
+          .withOccurrences(0).expect {
+            manualTime.timePasses(9.seconds)
+          }
+      }
+
+      s"close down when it's time to close" in {
+        LoggingTestKit.info(closeLog).expect {
+          manualTime.timePasses(1.seconds)
+        }
+      }
+
+      "stay closed for at least 9 seconds" in {
+        LoggingTestKit
+          .info(openLog)
+          .withOccurrences(0).expect {
+            manualTime.timePasses(9.seconds)
+          }
+      }
+
+      "re-open after being closed for a while" in {
+        LoggingTestKit.info(openLog).expect {
+          manualTime.timePasses(1.seconds)
+        }
       }
     }
-
-    val reportLog = "Time to produce a report."
-    s"log '${reportLog}' at info, when a report command is received" in {
-      LoggingTestKit.info(reportLog).expect {
-        rareBooks ! RareBooks.Report
-      }
-    }
-
   }
-
 }
