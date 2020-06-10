8e28ff1d3251c904533985ce1d4d0a577bced828
David Stevenson <david.35472@gmail.com>
2020-06-10 22:37:25 +0900

    make RareBooks appear to open/close on schedule

--- chapter4_001_messaging/src/main/scala/com/rarebooks/library/RareBooks.scala
+++ chapter4_001_messaging/src/main/scala/com/rarebooks/library/RareBooks.scala
@@ -1,7 +1,8 @@
 package com.rarebooks.library
 
+import scala.concurrent.duration.{ MILLISECONDS => Millis, FiniteDuration }
 import akka.actor.typed.{ ActorRef, Behavior }
-import akka.actor.typed.scaladsl.{ ActorContext, Behaviors }
+import akka.actor.typed.scaladsl.{ ActorContext, Behaviors, TimerScheduler }
 
 object RareBooks {
 
@@ -10,23 +11,27 @@ object RareBooks {
   private[library] case object Close extends Command
   private[library] case object Report extends Command
 
+  private case object TimerKey
+
   def apply(): Behavior[Command] = Behaviors.setup { context =>
     context.log.info("RareBooks started")
-    new RareBooks(context).open()
+    Behaviors.withTimers { timers => new RareBooks(context, timers).open() }
   }
 
 }
 
-class RareBooks(context: ActorContext[RareBooks.Command]) {
+class RareBooks(context: ActorContext[RareBooks.Command], timers: TimerScheduler[RareBooks.Command]) {
   import RareBooks._
 
   private def open(): Behavior[Command] =
     Behaviors.receiveMessage {
       case Open =>
         context.log.info("Time to open up!")
+        timers.startSingleTimer(TimerKey, Close, FiniteDuration(10000, Millis))
         Behaviors.same
       case Close =>
         context.log.info("Time to close!")
+        timers.startSingleTimer(TimerKey, Open, FiniteDuration(10000, Millis))
         Behaviors.same
       case Report =>
         context.log.info("Time to produce a report.")
