a12d903d94e030df5f7e8aabf41420d64262ef23
David Stevenson <david.35472@gmail.com>
2020-06-12 13:12:18 +0900

    Refactor RareBooks state management
    Eliminating vars and flipping between open() and closed() Behavior
    instead

--- chapter4_001_messaging/src/main/scala/com/rarebooks/library/RareBooks.scala
+++ chapter4_001_messaging/src/main/scala/com/rarebooks/library/RareBooks.scala
@@ -14,15 +14,13 @@ object RareBooks {
   private case object TimerKey
 
   def apply(): Behavior[Command] = Behaviors.setup { context =>
-    Behaviors.withTimers { timers => new RareBooks(context, timers).open() }
+    Behaviors.withTimers { timers => new RareBooks(context, timers).closed() }
   }
 
 }
 
 class RareBooks(context: ActorContext[RareBooks.Command], timers: TimerScheduler[RareBooks.Command]) {
   import RareBooks._
-
-  private var state: Int = 0
 
   private def init(): Unit = {
     context.log.info("RareBooks started")
@@ -33,20 +31,25 @@ class RareBooks(context: ActorContext[RareBooks.Command], timers: TimerScheduler
   private def open(): Behavior[Command] =
     Behaviors.receiveMessage {
       case Open =>
-        if (state == 0) {
-          context.log.info("Time to open up!")
-          timers.startSingleTimer(TimerKey, Close, FiniteDuration(10000, Millis))
-          state = 1
-        } else
-          context.log.info("We're already open.")
+        context.log.info("We're already open.")
         Behaviors.same
       case Close =>
-        if (state == 1) {
-          context.log.info("Time to close!")
-          timers.startSingleTimer(TimerKey, Open, FiniteDuration(10000, Millis))
-          state = 0
-        } else
-          context.log.info("We're already closed.")
+        context.log.info("Time to close!")
+        timers.startSingleTimer(TimerKey, Open, FiniteDuration(10000, Millis))
+        closed()
+      case Report =>
+        context.log.info("We only produce reports while closed")
+        Behaviors.same
+    }
+
+  private def closed(): Behavior[Command] =
+    Behaviors.receiveMessage {
+      case Open =>
+        context.log.info("Time to open up!")
+        timers.startSingleTimer(TimerKey, Close, FiniteDuration(10000, Millis))
+        open()
+      case Close =>
+        context.log.info("We're already closed.")
         Behaviors.same
       case Report =>
         context.log.info("Time to produce a report.")
