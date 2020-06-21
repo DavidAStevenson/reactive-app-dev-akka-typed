462335b18546f44f9e5334ec1e1f0bce8b14d072
David Stevenson <david.35472@gmail.com>
2020-06-21 22:03:45 +0900

    Make Librarian simulate research time
    Now the Librarian doesn't fulfil requests immediately, but simulates
    their taking some time to complete. It it done by use of timers. The
    Librarian schedules a timer to go off at which time the response to the
    request is then sent.

--- chapter4_001_messaging/src/main/scala/com/rarebooks/library/Librarian.scala
+++ chapter4_001_messaging/src/main/scala/com/rarebooks/library/Librarian.scala
@@ -2,7 +2,7 @@ package com.rarebooks.library
 
 import scala.concurrent.duration.FiniteDuration
 import akka.actor.typed.{ ActorRef, Behavior }
-import akka.actor.typed.scaladsl.{ ActorContext, Behaviors }
+import akka.actor.typed.scaladsl.{ ActorContext, Behaviors, TimerScheduler }
 
 object Librarian {
 
@@ -10,10 +10,13 @@ object Librarian {
 
   sealed trait PrivateCommand extends RareBooksProtocol.BaseMsg
   private[library] case class GetState(replyTo: ActorRef[PrivateResponse]) extends PrivateCommand
+  case class NotifyResearchResult(result: RareBooksProtocol.Msg, replyTo: ActorRef[RareBooksProtocol.Msg]) extends PrivateCommand
 
   sealed trait PrivateResponse extends RareBooksProtocol.BaseMsg
   private[library] case object Busy extends PrivateResponse
   private[library] case object Ready extends PrivateResponse
+
+  private case object TimerKey
 
   def optToEither[T](value: T, func: T => Option[List[BookCard]]):
     Either[BookNotFound, BookFound] =
@@ -28,13 +31,18 @@ object Librarian {
   }
 
   private[library] def setup(findBookDuration: FiniteDuration): Behavior[RareBooksProtocol.BaseMsg] =
-    Behaviors.setup[RareBooksProtocol.BaseMsg] { context =>
-      new Librarian(context, findBookDuration).ready()
+    Behaviors.withTimers { timers =>
+      Behaviors.setup[RareBooksProtocol.BaseMsg] { context =>
+        new Librarian(context, timers, findBookDuration).ready()
+      }
     }
 
 } 
 
-class Librarian(context: ActorContext[RareBooksProtocol.BaseMsg], findBookDuration: FiniteDuration) {
+class Librarian(
+  context: ActorContext[RareBooksProtocol.BaseMsg],
+  timers: TimerScheduler[RareBooksProtocol.BaseMsg],
+  findBookDuration: FiniteDuration) {
 
   context.log.info("Librarian started")
 
@@ -66,17 +74,23 @@ class Librarian(context: ActorContext[RareBooksProtocol.BaseMsg], findBookDurati
 
   protected def busy(): Behavior[RareBooksProtocol.BaseMsg] =
     Behaviors.receiveMessage {
+      case NotifyResearchResult(result, replyTo) =>
+        replyTo ! result
+        ready()
       case GetState(replyTo) =>
         replyTo ! Busy
-        ready()
+        Behaviors.same
       case other =>
         Behaviors.same
     }
 
+  private def notifyResultLater(msg: Msg, replyTo: ActorRef[Msg]): Unit =
+    timers.startSingleTimer(TimerKey, NotifyResearchResult(msg, replyTo), findBookDuration)
+
   private def process(result: Either[BookNotFound, BookFound], replyTo: ActorRef[Msg]): Unit = {
     result.fold (
-      bookNotFound => replyTo ! bookNotFound,
-      bookFound    => replyTo ! bookFound
+      bookNotFound => notifyResultLater(bookNotFound, replyTo),
+      bookFound    => notifyResultLater(bookFound, replyTo)
     )
   }
 
