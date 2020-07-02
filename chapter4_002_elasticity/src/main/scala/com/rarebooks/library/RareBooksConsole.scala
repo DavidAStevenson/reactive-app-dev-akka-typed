ed27f18034846f7147871c36384e5d814e3e2e0e
David Stevenson <david.35472@gmail.com>
2020-07-02 15:02:25 +0900

    print actor tree before system shutdown

--- chapter4_002_elasticity/src/main/scala/com/rarebooks/library/RareBooksConsole.scala
+++ chapter4_002_elasticity/src/main/scala/com/rarebooks/library/RareBooksConsole.scala
@@ -24,6 +24,7 @@ class RareBooksConsole(actorSystem: ActorSystem[Command]) extends Console {
         actorSystem ! CreateCustomer(count, odds, tolerance)
         commandLoop()
       case Command.Quit =>
+        println(actorSystem.printTree)
         actorSystem.terminate()
       case Command.Unknown(command) =>
         println(s"Unknown command $command")
